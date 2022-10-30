cd5273bcc1bc15a01c44035fb4d60929e74a3142
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-10-31 03:27:44 +0400

    refactored

--- script.js
+++ script.js
@@ -7,18 +7,8 @@ let arr = Object.values(img);
 const imgWiden = img.forEach((el, i) =>
   el.addEventListener("click", function () {
     img[i].classList.toggle("img-widen");
-    // img[i].classList.toggle("img-text");
-    // imgWidenAfter.style.color = "#fff";
-    // img.style.width = "19%";
-    console.log(el, i);
-    // img[i].style.width = "100%";
-    console.log(typeof img);
-    console.log(img);
-    console.log(arr);
     let spliced = arr.splice(i, 1);
-    console.log(spliced, arr);
     arr.forEach((el) => el.classList.toggle("img-small"));
     arr.splice(i, 0, ...spliced);
-    console.log(arr);
   })
 );
