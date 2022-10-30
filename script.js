f6de6a7ac8d6e51ffceba2019132350fcfdc916f
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-10-31 01:32:09 +0400

    imgWiden function added

--- script.js
+++ script.js
@@ -3,14 +3,20 @@
 let img = document.querySelectorAll(".img");
 let img1 = document.querySelector(".img1");
 
-img.forEach((el, i) =>
+console.log(img);
+let arr = Object.values(img);
+// let arr = Object.keys(img);
+// let arr = Object.entries(img);
+console.log(arr);
+const imgWiden = img.forEach((el, i) =>
   el.addEventListener("click", function () {
     console.log(el, i);
     img[i].style.width = "100%";
     console.log(typeof img);
     console.log(img);
-    let arr = Object.values(img);
     arr.splice(i, 1);
     console.log(arr);
+    arr.forEach((el) => (el.style.width = "5%"));
+    img[i].style.width = "75%";
   })
 );
